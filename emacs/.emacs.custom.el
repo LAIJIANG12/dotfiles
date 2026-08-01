e31879e88ff18ddc2134f56a029e74b7f98d7264
LJ <15521669172@163.com>
2026-08-01 19:11:42 +0800

    Update .emacs.custom.el

--- emacs/.emacs.custom.el
+++ emacs/.emacs.custom.el
@@ -1,12 +1,10 @@
+;;; -*- lexical-binding: t -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
- '(whitespace-style
-   '(face trailing tabs spaces lines indentation space-after-tab
-          space-mark tab-mark)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
