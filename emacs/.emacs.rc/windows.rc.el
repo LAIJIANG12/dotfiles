eb48bac226a8da7ee6ff2e4803f736045ddd41ba
LJ <15521669172@163.com>
2026-08-01 19:12:38 +0800

    Update windows.rc.el

--- emacs/.emacs.rc/windows.rc.el
+++ emacs/.emacs.rc/windows.rc.el
@@ -1,48 +1,19 @@
 ;;; Default PowerShell
-(setq shell-file-name "pwsh.exe")
-;; (setq shell-command-switch "-c")
+;; (setq shell-file-name "pwsh.exe")
 ;; (setq shell-command-switch "-Command")
-(with-eval-after-load 'comint
-  (define-key comint-mode-map (kbd "C-c l") 'comint-clear-buffer))
-
-;;; CMD
-(defun my-open-external-terminal-cmd ()
-  (interactive)
-  (let ((dir (expand-file-name (if (buffer-file-name)
-                                   (file-name-directory (buffer-file-name))
-                                default-directory))))
-    (w32-shell-execute "open" "cmd.exe"
-                       (concat "/K cd /d " (file-name-as-directory dir)))))
-;; (w32-shell-execute "open" "pwsh.exe"
-;;                    (concat "-NoExit -Command Set-Location '" dir "'"))
-
-(global-set-key (kbd "C-c t") 'my-open-external-terminal-cmd)
+;; (setq w32-quote-process-args t)
+;; (with-eval-after-load 'comint
+;;   (define-key comint-mode-map (kbd "C-c l") 'comint-clear-buffer))
 
 
-;;; PowerShell
 (defun my-open-external-terminal-pwsh ()
   (interactive)
-  (let* ((dir (expand-file-name (if (buffer-file-name)
-                                   (file-name-directory (buffer-file-name))
-                                 default-directory)))
-         (win-dir (replace-regexp-in-string "/" "\\" dir t t)))
-    (w32-shell-execute "open" "pwsh.exe"
-             (concat "-NoExit -Command \"Set-Location -Path '" win-dir "'\""))))
+  (let ((dir (expand-file-name (if (buffer-file-name) (file-name-directory (buffer-file-name)) default-directory))))
+    (w32-shell-execute "open" "pwsh.exe" (concat "-WorkingDirectory \"" dir "\""))))
 
-(global-set-key (kbd "C-c r") 'my-open-external-terminal-pwsh)
+(global-set-key (kbd "C-c r") #'my-open-external-terminal-pwsh)
 
 ;;; Invoke the software
 (defun open-firefox ()
     (interactive)
     (call-process "C:\\Program Files\\Mozilla Firefox\\firefox.exe" nil 0))
-
-
-(defun open-Krita ()
-    (interactive)
-  (call-process "D:\\Laijiang\\Krita (x64)\\bin\\krita.exe" nil 0))
-
-
-;;; Specify the location
-(setenv "PYTHONUTF8" "1")
-(setq python-shell-interpreter "D:/opt/Python/python.exe")
-(setq python-shell-pip-command "D:/opt/Python/Scripts/pip.exe")
