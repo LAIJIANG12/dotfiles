6c09bcf1d0aa0df17837a10023d49f6a52ba1ac1
LJ <15521669172@163.com>
2026-09-18 18:53:25 +0800

    Update misc.rc.el

--- emacs/.emacs.rc/misc.rc.el
+++ emacs/.emacs.rc/misc.rc.el
@@ -1,4 +1,5 @@
-(setq ansi-color-for-compilation-mode t)
+(require 'ansi-color)
+(add-hook 'compilation-filter-hook #'ansi-color-compilation-filter)
 
 (setq-default inhibit-splash-screen t
               make-backup-files nil
@@ -7,35 +8,36 @@
               tab-width 4
               indent-tabs-mode nil
               compilation-scroll-output t
-              visible-bell (equal system-type 'windows-nt)
               )
 
 ;;; Force all windows to prioritize horizontal splitting
-(setq split-height-threshold 0)
-(setq split-width-threshold nil)
+(setq split-height-threshold nil)
+(setq split-width-threshold 0)
 
-;;; hippie-expand
-(setq hippie-expand-try-functions-list
-      '(try-expand-dabbrev
-        try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill
-        try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-all-abbrevs
-        try-expand-list
-        try-expand-line
-        try-complete-lisp-symbol-partially
-        try-complete-lisp-symbol
-        ))
-(global-set-key (kbd "M-/") #'hippie-expand)
-(setq hippie-expand-ignore-case t)
-(setq hippie-expand-verbose nil)
+;;; hippie-exp
+(use-package hippie-exp
+  :bind ("M-/" . hippie-expand)
+  :config
+  (setq hippie-expand-try-functions-list
+        '(try-expand-dabbrev
+          try-expand-dabbrev-all-buffers
+          try-complete-file-name-partially
+          try-complete-file-name
+          try-expand-dabbrev-from-kill
+          try-complete-lisp-symbol-partially  
+          try-complete-lisp-symbol      
+          try-expand-all-abbrevs        
+          try-expand-list               
+          try-expand-line))                
+
+  (setq hippie-expand-ignore-case t
+        hippie-expand-verbose nil))
 
 ;;; replace-regexp
 (global-set-key (kbd "C-c %") 'replace-regexp)
 
 ;; Window navigation shortcuts
-(windmove-default-keybindings)
+(windmove-default-keybindings 'super)
 
 ;;; File copying
 (defun rc/buffer-file-name ()
@@ -69,6 +71,7 @@
 (global-set-key (kbd "C-c z") #'rc/put-file-name-on-clipboard)
 (global-set-key (kbd "C-c b") #'rc/put-buffer-name-on-clipboard)
 
+;;; #Color
 (use-package rainbow-mode
   :ensure t
   :hook (prog-mode . rainbow-mode)
